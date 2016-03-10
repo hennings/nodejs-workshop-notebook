b5a81b5dd555b123795939d24bc2ff090e54286d
Henning Spjelkavik <henning@spjelkavik.net>
2016-03-10 10:55:08 +0100

    Middleware.

--- book/index.js
+++ book/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var logIt = function (a) {
+    return function (req, res, next) {
+        console.log(a + " incoming request logged at " + new Date())
+        next();
+    }
+}
+
+var auth = function (req, res, next) {
+    console.log("auth")
+    next();
+}
+
+app.use(logIt("first"));
+app.use(auth);
+
+app.get('/', logIt("second"), function (req, res) {
+    res.send('Hello World!');
+    console.log("served hello world")
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
