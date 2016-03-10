a6e5f298cf1c6f0df090c11952767c7ca61fa777
Henning Spjelkavik <henning@spjelkavik.net>
2016-03-10 09:39:46 +0100

    First task.

---
+++ Programmer.js
@@ -0,0 +1,33 @@
+
+function Programmer() {
+    this.languages = [];
+}
+
+var programmer = new Programmer();
+
+Programmer.prototype.learnNewLanguage = function(lang) {
+    this.languages.push(lang);
+};
+
+Programmer.prototype.isPragmatic = function() {
+    return this.languages.length >= 3 ;
+};
+
+var programmer = new Programmer();
+
+
+programmer.learnNewLanguage("clojure");
+programmer.learnNewLanguage("java");
+console.log(programmer.isPragmatic());
+programmer.learnNewLanguage("cobol");
+console.log(programmer.isPragmatic());
+
+
+/*
+
+var p2 = new Programmer();
+["clojure","java","cobol"].forEach(
+    p2.learnNewLanguage
+);
+
+*/
