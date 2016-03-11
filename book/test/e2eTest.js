f0f2c7622b8ea6515fbc943d8fb6421d6b484d3a
Henning Spjelkavik <henning@spjelkavik.net>
2016-03-11 08:49:43 +0100

    Working copy.

--- book/test/e2eTest.js
+++ book/test/e2eTest.js
@@ -1,23 +1,23 @@
-var request = require("supertest")
-var assert = require("assert")
-var inmemory = require("../inmemory.js")
-var app = require("../app.js")(inmemory)
+var request = require('supertest');
+var assert = require('assert');
+var inMemoryRepository = require('../inMemoryRepository');
+var app = require('../app.js')(inMemoryRepository);
 
-describe("Book inventor", function () {
-    it("allows to stock up the items", function (done) {
-        request(app)
-            .post("/stock")
-            .send({
-                "isbn": "1234",
+describe('Book inventory', function () {
+    it('allows to stock up the items', function (done) {
+        request(app).
+            post('/stock').
+            send({
+                "isbn": "1234567890",
                 "count": 10
-            }).set("Content-Type", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .end(function (err, res) {
-                    if (err) return done(err);
-                    assert.equal(res.body.isbn, "1234")
-                    done();
-                }
-            )
+            }).
+            set('Content-Type', 'application/json').
+            expect('Content-Type', /json/).
+            expect(200).
+            end(function (err, res) {
+                if (err) return done(err);
+                assert.equal(res.body.isbn, "1234567890");
+                done();
+            });
     })
-})
+});
